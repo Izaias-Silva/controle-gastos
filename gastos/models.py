d9215ba7c71d22c9cc975ff65a369d70a513b1d1
Izaias silva <2003silva.izaias@gmail.com>
2026-08-20 21:51:00 -0300

    migracoes feitas

--- gastos/models.py
+++ gastos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def _str_(self):
+        return self.nome
+
+class Gastos(models.Model):
+    descricao = models.CharField(max_length=200)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(User, on_delete=models.CASCADE)
+    data = models.DateField()
+
+    def _str_(self):
+        return f"{self.descricao} - R$ {self.valor}"
+
+
