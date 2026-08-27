c2ffa3503ef4dbe0fd90b65e084231af952d81fe
Your Name <joaop.jp75@gmail.com>
2026-08-26 21:01:49 -0300

    cha models categoria e gasto

--- gastos/models.py
+++ gastos/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class categoria(models.Model):
+class Categoria(models.Model):
     nome = models.CharField(max_length=100)
 
     def _str_(self):
         return self.nome
 
-class Gastos(models.Model):
+class Gasto(models.Model):
     descricao = models.CharField(max_length=200)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.ForeignKey(User, on_delete=models.CASCADE)
